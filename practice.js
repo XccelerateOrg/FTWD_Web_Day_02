25a02dd2b81fae65220997ea210852d54393a2a3
Elliott Ng <68053981+elliott9710@users.noreply.github.com>
2020-08-28 14:18:00 +0800

    updating

--- practice.js
+++ practice.js
@@ -1,3 +1,16 @@
 // Your Javascript practice exercise goes here
 
 // Please add your if the statements are true or false here!
+x = 8;
+y = 3;
+
+x == y; 
+//false
+x && y < 10; 
+// true
+ x || y =< 8 
+ // error
+y === "3"; 
+// false (not a string)
+!(x == 5); 
+// true
