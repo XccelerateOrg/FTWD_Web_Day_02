827b7ee07b89f50ff5db4a1c943181c1ad7e61dc
JPoodle <jaredpoole@gmail.com>
2020-10-30 14:10:25 +0800

    Answers to practice exercise

--- practice.js
+++ practice.js
@@ -1,3 +1,13 @@
 // Your Javascript practice exercise goes here
 
+let x = 8;
+let y = 3;
+
+x == y // ==> False
+x && y < 10 //==> True
+x || y <= 8 //==> True
+y === '3' //==>False
+!(x == 5) //==>True
+
 // Please add your if the statements are true or false here!
+
