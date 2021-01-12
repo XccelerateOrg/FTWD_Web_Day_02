b1a1ec4f8006322bc58d584d8045e386ff162c93
Philemon Ip <philemonnip@gmail.com>
2021-01-12 15:00:58 +0800

    Exercise submission by Phil

--- practice.js
+++ practice.js
@@ -1,3 +1,14 @@
 // Your Javascript practice exercise goes here
 
 // Please add your if the statements are true or false here!
+let x = 8
+let y = 3
+
+console.log(x&&y)
+
+x == y => false 
+x && y < 10 => true
+x || y <= 8 => true
+y === '3' => false
+!(x == 5) => true
+
