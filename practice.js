451018b8192f74e444b3c05537df147ed043d0de
JamesMB209 <jmb209@gmail.com>
2021-06-01 18:14:56 +0800

    Completed practice tasks

--- practice.js
+++ practice.js
@@ -1,3 +1,10 @@
 // Your Javascript practice exercise goes here
 
 // Please add your if the statements are true or false here!
+x = 8 and y = 3.
+
+x == y // False
+x && y < 10 //True
+x || y <= 8 //True
+y === '3' // False
+!(x == 5) //True
