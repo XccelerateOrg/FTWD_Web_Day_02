3a5660d9a7c55b1decddee18ccdcc953bdd0eb86
joycelkl <joycelkl@hotmail.com>
2021-06-01 23:13:57 +0800

    really completed

--- practice.js
+++ practice.js
@@ -1,3 +1,16 @@
 // Your Javascript practice exercise goes here
 
 // Please add your if the statements are true or false here!
+
+let x = 8;
+let y = 3;
+
+x == y  //=>false;
+x && y < 10 //=>true;
+x || y <= 8 //=>true;
+y === '3' //=>false;
+!(x == 5) //=>true;
+
+function area(height, width) {
+    return height * width;
+}
