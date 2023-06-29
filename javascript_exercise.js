d6e1bbf810523d18fd59a55e121db67b2b9bc0c0
Laxman <laxmanketheth76@gmail.com>
2023-06-29 23:31:47 +0800

    homework

--- javascript_exercise.js
+++ javascript_exercise.js
@@ -1,5 +1,8 @@
 // The function below is not working properly - can you fix it?
 
-function area(height width) {
-    height * width;
+function area(height, width) {
+    return height * width;
 }
+
+
+console.log(area(12,10)); // this is just to check if function is working//
