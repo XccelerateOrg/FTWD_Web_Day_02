b24b19893969cee441c169e36a1ab704631891fe
EllisCallaghan <42118364+EllisCallaghan@users.noreply.github.com>
2023-08-18 17:47:17 +0800

    Delete FTWD_Web_Day_02-master directory

--- FTWD_Web_Day_02-master/javascript_exercise.js
+++
@@ -1,8 +0,0 @@
-// The function below is not working properly - can you fix it?
-
-function area(height, width) {
-    const area = height * width;
-    console.log(area)
-}
-
-area(3,5)
--- FTWD_Web_Day_02-master/practice.js
+++
@@ -1,3 +0,0 @@
-// Your Javascript practice exercise goes here
-
-// Please add your if the statements are true or false here!
